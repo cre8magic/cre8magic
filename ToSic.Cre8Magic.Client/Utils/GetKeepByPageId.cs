692ee733c6b04d2e31732f656626866294f4c88b
iJungleboy <daniel.mettler@2sic.com>
2024-11-15 16:59:15 +0100

    minor improve GetKeepByPageId

--- ToSic.Cre8Magic.Client/Utils/GetKeepByPageId.cs
+++ ToSic.Cre8Magic.Client/Utils/GetKeepByPageId.cs
@@ -6,6 +6,10 @@ public class GetKeepByPageId<T> where T : class
 {
     public T Get(PageState pageState, Func<T> create)
     {
+        if (pageState == null)
+            throw new ArgumentNullException(nameof(pageState),
+                $"This code was called to early, before {nameof(PageState)} was ready. Make sure you only access this in output code, not in constructors etc.");
+
         return _cache.Get(
             () => (create(), pageState.Page.PageId),
             (_, i) => i == pageState.Page.PageId
