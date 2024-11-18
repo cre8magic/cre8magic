4abd8ac154f13e906ede6c62f0fe414a32ea7735
iJungleboy <daniel.mettler@2sic.com>
2024-11-18 20:56:59 +0100

    minor

--- ToSic.Cre8Magic.Client/Components/MagicComponentBase.razor.cs
+++ ToSic.Cre8Magic.Client/Components/MagicComponentBase.razor.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Oqtane.UI;
-using ToSic.Cre8magic.Themes.Internal;
 
 namespace ToSic.Cre8magic.Components;
 
@@ -10,17 +9,6 @@ namespace ToSic.Cre8magic.Components;
 public abstract class MagicComponentBase: ComponentBase
 {
     [CascadingParameter]
-    protected PageState PageState { get; set; }
+    public required PageState PageState { get; set; }
 
-    [Inject]
-    public IMagicFactoryWip MagicFactory { get; set; }
-
-    //private MagicThemeDesigner Designer => _designer ??= MagicFactory.ThemeDesigner(PageState);
-    //private MagicThemeDesigner? _designer;
-
-    //public string? Classes(string target) => Designer.Classes(target);
-
-    //public string? Value(string target) => Designer.Value(target);
-
-    //public string? Id(string name) => Designer.Id(name);
 }
