86a909da728219ed8c70b6e64877836ab53f6612
iJungleboy <daniel.mettler@2sic.com>
2025-01-10 10:32:21 +0100

    TestTheme - move constants into own file

---
+++ ToSic.Cre8Magic.OqtaneBs5.Client/OqtaneBasic/MagicTheme.razor.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Components;
+using ToSic.Cre8magic.Act;
+using ToSic.Cre8magic.Themes;
+using ToSic.Cre8magic.Utils;
+
+namespace ToSic.Cre8magic.OqtaneBasic;
+
+/// <summary>
+/// Base class for our themes. It's responsible for
+///
+/// 1. Some basic properties such as Name, BodyClasses etc. which each theme can configure
+/// 2. Adding special classes to the body tag so that the CSS can best optimize for each scenario
+/// </summary>
+/// <remarks>
+/// - The base class must be abstract, so that Oqtane doesn't see it as a real them.
+/// - The config-properties must be abstract, so the inheriting files are forced to set them. 
+/// </remarks>
+public abstract class MagicTheme : Oqtane.Themes.ThemeBase
+{
+    /// <summary>
+    /// Name to show in the Theme-picker.
+    /// Must be set by each inheriting theme, which is why it's marked abstract to enforce this.
+    /// </summary>
+    public abstract override string Name { get; }
+
+    /// <summary>
+    /// This contains the default settings which must be used in this theme.
+    /// Any inheriting class must specify what it will be. 
+    /// </summary>
+    public abstract MagicThemePackage ThemePackage { get; }
+
+    /// <summary>
+    /// The Theme Kit which is help the theme become awesome.
+    /// </summary>
+    public IMagicThemeKit ThemeKit => _themeKit.Get(PageState, () => MagicAct.ThemeKit(new() { PageState = PageState }));
+    private readonly GetKeepByPageId<IMagicThemeKit> _themeKit = new();
+
+    /// <summary>
+    /// The <see cref="MagicAct"/> which coordinates everything.
+    /// </summary>
+    /// <remarks>
+    /// It's provided by Dependency Injection, required.
+    /// </remarks>
+    [Inject]
+    public required IMagicAct MagicAct
+    {
+        get => _magicAct;
+        [MemberNotNull(nameof(_magicAct))]
+        set => _magicAct = value.UseThemePackage(ThemePackage);
+    }
+    private IMagicAct _magicAct;
+
+    /// <summary>
+    /// Handle OnParametersSet to provide the latest PageState to the MagicAct.
+    /// </summary>
+    /// <remarks>
+    /// OnParametersSet runs whenever any parameter changes - such as PageState.
+    /// It also runs before OnParametersSetAsync.
+    /// </remarks>
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+        // Provide the latest PageState on every change
+        MagicAct.UsePageState(PageState);
+    }
+
+
+    /// <summary>
+    /// Handle OnAfterRenderAsync to track page views
+    /// </summary>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+        await MagicAct.AnalyticsKit().TrackPage(firstRender);
+    }
+}
---
+++ ToSic.Cre8Magic.OqtaneBs5.Client/OqtaneBs5/MagicTheme.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
+using ToSic.Cre8magic.Act;
+using ToSic.Cre8magic.Themes;
+using ToSic.Cre8magic.Utils;
+
+namespace ToSic.Cre8magic.OqtaneBs5;
+
+// Note: this is basically duplicate code from the OqtaneBasic.MagicTheme
+// but we want to be sure that all the properties are in the docs
+// which can't be done with inheritance
+
+/// <inheritdoc cref="OqtaneBasic.MagicTheme" />
+public abstract class MagicTheme: Oqtane.Themes.ThemeBase
+{
+    /// <inheritdoc cref="OqtaneBasic.MagicTheme.Name" />
+    public abstract override string Name { get; }
+
+    /// <inheritdoc cref="OqtaneBasic.MagicTheme.ThemePackage" />
+    public abstract MagicThemePackage ThemePackage { get; }
+
+
+    /// <inheritdoc cref="OqtaneBasic.MagicTheme.ThemeKit" />
+    public IMagicThemeKit ThemeKit => _themeKit.Get(PageState, () => MagicAct.ThemeKit(new() { PageState = PageState }));
+    private readonly GetKeepByPageId<IMagicThemeKit> _themeKit = new();
+
+    /// <inheritdoc cref="OqtaneBasic.MagicTheme.MagicAct" />
+    [Inject]
+    public required IMagicAct MagicAct
+    {
+        get => _magicAct;
+        [MemberNotNull(nameof(_magicAct))]
+        set => _magicAct = value.UseThemePackage(ThemePackage);
+    }
+    private IMagicAct _magicAct;
+
+    /// <inheritdoc cref="OqtaneBasic.MagicTheme.OnParametersSet" />
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+        // Provide the latest PageState on every change
+        MagicAct.UsePageState(PageState);
+    }
+
+    /// <inheritdoc cref="OqtaneBasic.MagicTheme.OnAfterRenderAsync" />
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+        await MagicAct.AnalyticsKit().TrackPage(firstRender);
+    }
+}
