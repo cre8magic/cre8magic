e429f06bdc2d680fcfa9d74f394675b0808fb7f6
Tonći Vatavuk <tonci@sistemi.hr>
2025-02-11 12:28:35 +0100

    updated ToSic.Cre8magic.Oqtane.nuspec

--- ToSic.Cre8magic.Package/release.cmd
+++ ToSic.Cre8magic.Package/release.cmd
@@ -1,7 +1,7 @@
-@echo off
+@echo on
 set TargetFramework=%1
 set ProjectName=%2
 
 del "*.nupkg"
-"..\..\..\oqtane.framework\oqtane.package\nuget.exe" pack %ProjectName%.nuspec -Properties targetframework=%TargetFramework%;projectname=%ProjectName%
+"..\..\oqtane.framework\oqtane.package\nuget.exe" pack ToSic.Cre8magic.Oqtane.nuspec -Properties targetframework=%TargetFramework%;projectname=%ProjectName%
 XCOPY "*.nupkg" "..\..\..\oqtane.framework\Oqtane.Server\wwwroot\Packages\" /Y
