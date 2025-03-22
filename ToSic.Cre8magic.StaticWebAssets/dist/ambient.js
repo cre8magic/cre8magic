1eeeed12d11063c02f045b31ecc55de9e19f14d5
iJungleboy <daniel.mettler@2sic.com>
2025-03-22 22:03:43 +0100

    fix ambient js to work correctly with window.bootstrap

--- ToSic.Cre8magic.StaticWebAssets/dist/ambient.js
+++ ToSic.Cre8magic.StaticWebAssets/dist/ambient.js
@@ -2,25 +2,23 @@ const d = "theme", r = `${d}-breadcrumbs`;
 function m() {
   console.log("initMailDecrypt"), setTimeout(function() {
     const e = document.querySelectorAll("[data-madr1]:not(.madr-done)");
-    Array.from(e).forEach((t, n) => {
-      const o = t.getAttribute("data-madr1") + "@" + t.getAttribute("data-madr2") + "." + t.getAttribute("data-madr3"), i = t.getAttribute("data-linktext") ? t.getAttribute("data-linktext") : o, a = document.createElement("a");
-      a.setAttribute("href", `mailto:${o}`), a.innerHTML = i || "", t.parentElement && t.parentElement.appendChild(a), t.classList.add("madr-done"), t.style.display = "none";
+    Array.from(e).forEach((t, o) => {
+      const n = t.getAttribute("data-madr1") + "@" + t.getAttribute("data-madr2") + "." + t.getAttribute("data-madr3"), i = t.getAttribute("data-linktext") ? t.getAttribute("data-linktext") : n, a = document.createElement("a");
+      a.setAttribute("href", `mailto:${n}`), a.innerHTML = i || "", t.parentElement && t.parentElement.appendChild(a), t.classList.add("madr-done"), t.style.display = "none";
     });
   }, 500);
 }
 function f() {
   console.log("initOffCanvasEvents");
-  var e = document.querySelectorAll(".mobile-navigation-link"), t = document.getElementById("offcanvasNavbar");
-  if (t) {
-    var n = new bootstrap.Offcanvas(t);
-    e.forEach((o) => {
-      o.addEventListener("click", () => {
-        n.hide();
-      });
-    });
-  }
+  const e = document.querySelectorAll(".mobile-navigation-link"), t = document.getElementById("offcanvasNavbar");
+  t && e.forEach((o) => v(o, t));
 }
-function p() {
+function v(e, t) {
+  console.log("hideNavbarAfterClick", e, t), e.addEventListener("click", () => {
+    new window.bootstrap.Offcanvas(t).hide();
+  });
+}
+function w() {
   console.log("init breadcrumbs"), s(), window.addEventListener("resize", s), document.addEventListener("scroll", s);
   var e = document.querySelector(`.${r}-trigger`);
   e != null && e.addEventListener("click", () => {
@@ -32,47 +30,47 @@ function s() {
   console.log("test 2dm 2024-11-25b");
   var e = document.getElementById(`${d}-page-navigation`);
   if (e != null) {
-    var t = e.offsetHeight, n = document.querySelector(`.${r}`);
-    if (!n)
+    var t = e.offsetHeight, o = document.querySelector(`.${r}`);
+    if (!o)
       return;
-    n.style.top = t + "px";
-    var o = n.getBoundingClientRect().top;
-    o <= t && scrollY != 0 ? n.classList.add("bg-light", "shadow") : n.classList.remove("bg-light", "shadow");
+    o.style.top = t + "px";
+    var n = o.getBoundingClientRect().top;
+    n <= t && scrollY != 0 ? o.classList.add("bg-light", "shadow") : o.classList.remove("bg-light", "shadow");
   }
 }
-function v() {
+function p() {
   document.querySelectorAll("a").forEach((e, t) => {
-    var n;
-    e.hasAttribute("href") && ((n = e.getAttribute("href")) != null && n.endsWith(".pdf")) && e.setAttribute("target", "_blank");
+    var o;
+    e.hasAttribute("href") && ((o = e.getAttribute("href")) != null && o.endsWith(".pdf")) && e.setAttribute("target", "_blank");
   });
 }
-const w = `${d}-to-top`, b = "visible";
-function h() {
-  console.log("init to top"), y();
-}
+const b = `${d}-to-top`, h = "visible";
 function y() {
-  console.log("toTopShowHideOnScroll"), document.addEventListener("scroll", E);
+  console.log("init to top"), E();
 }
 function E() {
-  var e = document.getElementById(w);
-  if (!e) return;
-  e.addEventListener("click", L);
-  const t = document.body.scrollTop > 200 || document.documentElement.scrollTop > 200;
-  e.classList.toggle(b, t);
+  console.log("toTopShowHideOnScroll"), document.addEventListener("scroll", L);
 }
 function L() {
+  var e = document.getElementById(b);
+  if (!e) return;
+  e.addEventListener("click", T);
+  const t = document.body.scrollTop > 200 || document.documentElement.scrollTop > 200;
+  e.classList.toggle(h, t);
+}
+function T() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
-function T(e) {
-  console.log("2dm test activate GTM"), function(t, n, o, i, a) {
+function A(e) {
+  console.log("2dm test activate GTM"), function(t, o, n, i, a) {
     t[i] = t[i] || [], t[i].push({ "gtm.start": (/* @__PURE__ */ new Date()).getTime(), event: "gtm.js" });
-    var l = n.getElementsByTagName(o)[0], c = n.createElement(o), u = "";
+    var l = o.getElementsByTagName(n)[0], c = o.createElement(n), u = "";
     c.async = !0, c.src = "https://www.googletagmanager.com/gtm.js?id=" + a + u, l.parentNode.insertBefore(c, l);
   }(window, document, "script", "dataLayer", e);
 }
-class A {
+class B {
   constructor() {
-    this.activate = T;
+    this.activate = A;
   }
   pageView() {
     console.log("gtm-interop - track page view"), g("event", "blazor_page_view");
@@ -82,13 +80,13 @@ window.dataLayer = window.dataLayer || [];
 function g(e, t) {
   console.log("gtm - gtag"), window.dataLayer.push(arguments);
 }
-function B() {
-  window.cre8magic = window.cre8magic || {}, window.cre8magic.gtm = window.cre8magic.gtm || new A(), window.gtag = window.gtag || g;
+function S() {
+  window.cre8magic = window.cre8magic || {}, window.cre8magic.gtm = window.cre8magic.gtm || new B(), window.gtag = window.gtag || g;
 }
-h();
-v();
-m();
+y();
 p();
+m();
+w();
 f();
-B();
+S();
 //# sourceMappingURL=ambient.js.map
--- ToSic.Cre8magic.StaticWebAssets/src/scripts/ambient/menu.ts
+++ ToSic.Cre8magic.StaticWebAssets/src/scripts/ambient/menu.ts
@@ -1,22 +1,32 @@
 import { debug } from '../shared/constants';
 
-declare const bootstrap: any;
+/**
+ * Global bootstrap variable on window.
+ * Usually not available until later.
+ */
+// declare const bootstrap: bootstrap;
 
 // Takes care of closing the offcanvas when clicking on a navigation link
 // The page is not reloaded, so the offcanvas does not close
 export function initOffCanvasEvents() {
 
-  if (debug) console.log('initOffCanvasEvents');
+  if (debug)
+    console.log('initOffCanvasEvents');
 
   //Offcanvas close on link click
-  var navLinks = document.querySelectorAll(".mobile-navigation-link");
-  var navOffcanvas = document.getElementById('offcanvasNavbar')
+  const navLinks = document.querySelectorAll(".mobile-navigation-link");
+  const navOffcanvas = document.getElementById('offcanvasNavbar') as HTMLElement;
   if (navOffcanvas) {
-    var bsNavOffcanvas = new bootstrap.Offcanvas(navOffcanvas)
-    navLinks.forEach(element => {
-      element.addEventListener('click', () => {
-        bsNavOffcanvas.hide();
-      })
-    });
+    navLinks.forEach(element => hideNavbarAfterClick(element, navOffcanvas));
   }
+}
+
+function hideNavbarAfterClick(element: Element, navOffcanvas: HTMLElement) {
+  if (debug)
+    console.log('hideNavbarAfterClick', element, navOffcanvas);
+
+  element.addEventListener('click', () => {
+    const bsNavOffcanvas = new window.bootstrap.Offcanvas(navOffcanvas)
+    bsNavOffcanvas.hide();
+  });
 }
