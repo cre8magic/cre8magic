e6c0c55de0daa856a9dee620a5a00ba900acc6a0
Tonći Vatavuk <tonci@sistemi.hr>
2025-03-24 21:21:49 +0100

    build and distribute style.css from StaticWebAssets

--- ToSic.Cre8magic.StaticWebAssets/vite.config.ts
+++ ToSic.Cre8magic.StaticWebAssets/vite.config.ts
@@ -3,34 +3,47 @@ import { resolve } from 'path';
 import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
-    build: {
-        sourcemap: true,
-        outDir: 'dist',
-        minify: 'esbuild',
-        lib: {
-            entry: {
-                interop: resolve(__dirname, 'src/scripts/interop/interop.ts'),
-                ambient: resolve(__dirname, 'src/scripts/ambient/index.ts')
-            },
-            formats: ['es']
+  build: {
+    sourcemap: true,
+    outDir: 'dist',
+    minify: 'esbuild',
+    cssCodeSplit: false,
+    lib: {
+      entry: {
+        interop: resolve(__dirname, 'src/scripts/interop/interop.ts'),
+        ambient: resolve(__dirname, 'src/scripts/ambient/index.ts'),
+        style: resolve(__dirname, 'src/styles/style.scss')
+      },
+      formats: ['es']
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith('.css')) return 'style.min.css';
+          if (assetInfo.name?.endsWith('.css.map')) return 'style.min.css.map';
+          return '[name][extname]';
         },
-        rollupOptions: {
-            output: {
-                entryFileNames: '[name].js',
-            },
-            plugins: [
-                // Copy all files in dist to the OqtaneStaticAssetsPath after build
-                copy({
-                    targets: [
-                        {
-                            src: 'dist/**/*',
-                            dest: '../../oqtane.framework/Oqtane.Server/wwwroot/_content/ToSic.Cre8magic.Oqtane'
-                        }
-                    ],
-                    hook: 'writeBundle'
-                })
-            ]
-        }
+      },
+      plugins: [
+        // Copy all files in dist to the OqtaneStaticAssetsPath after build
+        copy({
+          targets: [
+            {
+              src: 'dist/**/*',
+              dest: '../../oqtane.framework/Oqtane.Server/wwwroot/_content/ToSic.Cre8magic.Oqtane'
+            }
+          ],
+          hook: 'writeBundle'
+        })
+      ]
     }
+  },
+  // css: {
+  //   devSourcemap: true,
+  //   postcss: {
+  //     map: { inline: false },
+  //     plugins: []
+  //   }
+  // },
 });
 
